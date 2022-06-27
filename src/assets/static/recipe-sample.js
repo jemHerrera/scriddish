aa4cb4fb0376833604a934182f2d6c7f31420c2f
Jem Herrera <jhneramuz@gmail.com>
2022-06-26 23:22:14 -0600

    added few more recipes

---
+++ src/assets/static/recipe-sample.js
@@ -0,0 +1,50 @@
+{
+    "id": "",
+    "title": "",
+    "image": "",
+    "description": "",
+    "category": "featured",
+    "tags": ["beef", "western", "featured"],
+    "time": "35 mins",
+    "ingredients":{
+        "Meat & Seafood": {
+            "ground-beef": {
+                "name": "Ground Beef",
+                "amount": 250,
+                "unit": "g"
+            }
+        },
+        "Produce": {
+        },
+        "Condiments & Spices":{
+        },
+        "Canned & Sauces": {
+        },
+        "Eggs & Dairy": {
+        },
+        "Others": {
+        }
+    },
+    "instructions":[
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        ""
+    ]
+}
+
+//hoisin glazed pork meatballs
+// figgy jam stuffed chicken
+// Breaded Mayo chicken
+// pork meatball minestrone
+// bacon corn chowder
+//seafood scampi with rice
+// pork and parmesan patties
+// pork and zucchini bolognese
+// steak spiced pork tenderloin
+// Shrimp scampi pasta
+// sweet and sour meatballs
